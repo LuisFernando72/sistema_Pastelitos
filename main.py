3348087d510bf97d201683031db460a8b011b17f
Luis9092 <luisc522txe@gmail.com>
2023-09-24 10:45:04 -0600

    terricolas

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def root():
     return render_template("index.html")
 
-
+#BUENOS DIAS, LES SALUDA UN TERRRICOLA, ENVIADO DESDE LA TIERRA
 
 if __name__ == "__main__":
     app.run(debug=True)

2ac7f60c70e80ccea5045674b9a7c00168403c40
BrendaPJ <xiomarajolon2000@gmail.com>
2023-09-27 16:22:40 -0500

    Modificado

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def root():
-    return render_template("index.html")
+    return render_template("index.html")#RETORNA EL HTML
 
 #BUENOS DIAS, LES SALUDA UN TERRRICOLA, ENVIADO DESDE LA TIERRA
 #COMPRAS PAS, DOS TRES CUATRO CINCOCLS

ded1a6b2530848a1ceec2d41f8f9bdd9767010a5
BrendaPJ <xiomarajolon2000@gmail.com>
2023-10-26 22:25:38 -0500

    Brenda-Avance

--- main.py
+++ main.py
@@ -21,7 +21,6 @@ def productos_render():
 
     return render_template("productos.html", tabla=retorno)
 
-
 @app.route("/operacionesProductos/<string:accion>", methods=["POST"])
 def crearProducto(accion):
     tiempo = datetime.now()
@@ -65,6 +64,81 @@ def crearProducto(accion):
 @app.route("/rebajas")
 def rebajas():
     return render_template("rebajas.html")
+
+# INICIO AUTENTICACION
+
+@app.route("/loguearse")
+def login():
+    return render_template("autenticarse.html")
+
+@app.route("/CrearCuenta", methods=["POST"])
+def CrearCuenta():
+
+    nombres = request.form["nombres"]
+    correo = request.form["correo"]
+    apellidos = request.form["apellidos"]
+    password = request.form["password"]
+    fecha = ""
+    idrol = 0
+
+    url = "http://127.0.0.1:8000/api/CrearUsuario"
+    response = {}
+    parametros = {
+        "id": 0,
+        "nombres": nombres,
+        "correo": correo,
+        "apellidos": apellidos,
+        "password": str(password),
+        "fecha": fecha,
+        "idrol": idrol
+    }
+    x = requests.post(url, data=json.dumps(parametros))
+    if x.status_code == 201:
+        response = {"estado": 1,
+                    "mensaje": "Usuario Creado correctamente, ya puede iniciar sesión"}
+    else:
+        response = {"estado": 0, "mensaje": "Porfavor verificar los datos"}
+
+    return response
+
+
+@app.route("/iniciarSesion", methods=["POST"])
+def IniciarSesion():
+    correo = request.form["correo"]
+    password = request.form["password"]
+
+    per.Autenticar(str(correo), str(password))
+    response = per.ObtenerDatosMenu()
+    print("inicio")
+    print(response["estado"])
+
+    if response["estado"] == 1:
+        per.ObtenerMenu(str(response["mensaje"]))
+
+    return json.dumps(response)
+
+
+@app.route("/menu")
+def menu():
+
+    RenderMenu = per.menupintar
+   
+
+    return render_template("menu.html", ver2=RenderMenu)
+
+@app.route("/Clientes")
+def Clientes():
+    RenderMenu = per.menupintar
+    c = clientes()
+    retorno = c.listarClientes()
+    
+    return render_template("clientes.html", ver2=RenderMenu, tabla = retorno)
+
+
+@app.route("/CerrarSesion")
+def cerrar_sesion():
+    session.clear()
+    return redirect("/")
 
 
 if __name__ == "__main__":
